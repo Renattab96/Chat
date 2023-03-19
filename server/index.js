deb74a2d57eca651b4bb6605123dc3fcb2e8f51b
renattab96 <renattabenitez96@gmail.com>
2023-03-19 16:31:14 -0300

    Estructura del server

---
+++ server/index.js
@@ -0,0 +1,38 @@
+import express from 'express'
+import morgan from 'morgan'
+import {Server as SocketServer} from 'socket.io'
+import http from 'http'
+import cors from 'cors'
+import { PORT } from './config.js'
+
+
+
+const app = express()
+const server = http.createServer(app)
+const io =new SocketServer(server, {
+    cors:{
+        origin:'http://localhost:3000',
+        // credentials:true 
+    }
+  
+    
+})
+
+app.use(cors())
+app.use(morgan('dev'))
+
+io.on('connection',(socket)=>{
+    console.log(socket.id)
+    // console.log('un usuario conectado')
+    socket.on('message',(message)=>{
+        console.log(message)
+        socket.broadcast.emit('message',{
+            body: message,
+            from:socket.id,
+        })
+    })
+})
+
+
+server.listen(PORT)
+console.log('Server started on port ',PORT)
